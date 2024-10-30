2a446dca9ea61b9ce67d2919ecd46d904b2a8e5f
netow2005 <netodnet@gmail.com>
2024-10-29 22:16:31 -0300

    Estilizando a navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,22 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Box, Flex, Button } from "@chakra-ui/react";
 import { FaCalendarAlt, FaPhoneAlt, FaBox, FaShoppingCart } from "react-icons/fa";
 
 function NavBar() {
+
+  const location = useLocation();
+
   return (
+    
     <Box p={5}>
       <Flex justify="space-between" mb={5}>
         <Flex gap={5}>
             
-          <Button variant="link" leftIcon={<FaCalendarAlt />} colorScheme="red">
+            <Link to="/agendamento">
+          <Button variant="link" leftIcon={<FaCalendarAlt />} colorScheme={location.pathname === "/agendamento" ? "red" : "gray"}>
             Agenda
           </Button>
+          </Link>
+
           <Link to="/contato">
-          <Button variant="link" leftIcon={<FaPhoneAlt />}>
+          <Button variant="link" leftIcon={<FaPhoneAlt />} colorScheme={location.pathname === "/contato" ? "red" : "gray"}>
             Contato
           </Button>
           </Link>
+
           <Button variant="link" leftIcon={<FaBox />}>
             Estoque
           </Button>
